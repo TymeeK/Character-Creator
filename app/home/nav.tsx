464561a2927b954630e02c9c891367ac43efed02
Tymee Kong <tymeekong562@gmail.com>
2024-03-16 00:02:06 -0700

    Added avatar component and placeholder image

--- app/home/nav.tsx
+++ app/home/nav.tsx
@@ -7,20 +7,33 @@ import {
   NavigationMenuList,
   NavigationMenuTrigger,
 } from '@/components/ui/navigation-menu'
+import Logo from '../../Images/logo.png'
 import React from 'react'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 
-const Nav = () => {
+const Nav: React.FC = () => {
   return (
-    <NavigationMenu>
-      <NavigationMenuList>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <NavigationMenuLink>Link</NavigationMenuLink>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-      </NavigationMenuList>
-    </NavigationMenu>
+    <div className='flex w-screen bg-purple-100'>
+      <div className='w-1/2 '>
+        <Avatar className=''>
+          <AvatarImage src='https://github.com/shadcn.png' />
+          <AvatarFallback>Logo</AvatarFallback>
+        </Avatar>
+        <h1 className='inline'>Company Name</h1>
+      </div>
+      <div className='w-1/2 bg-white'>
+        <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+              <NavigationMenuContent>
+                <NavigationMenuLink>Link</NavigationMenuLink>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+      </div>
+    </div>
   )
 }
 
