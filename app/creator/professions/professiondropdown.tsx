5aba2374593fdd36d28d222fc1ed7fa97092319e
Tymee Kong <tymeekong562@gmail.com>
2024-03-15 17:17:18 -0700

    Centered Professions and Dropdown

--- app/creator/professions/professiondropdown.tsx
+++ app/creator/professions/professiondropdown.tsx
@@ -7,27 +7,32 @@ import {
   SelectItem,
 } from '@/components/ui/select'
 import React from 'react'
-import { professions } from '@/Delta Green/Types/professions'
+import { professions, Professions } from '@/Delta Green/Types/professions'
 
 const ProfessionDropDown = () => {
   return (
     <>
-      <Select>
-        <SelectTrigger>
-          <SelectValue placeholder='Professions' />
-        </SelectTrigger>
-        <SelectContent>
-          {professions.map((profession, index: number) => {
-            return (
-              <React.Fragment key={index}>
-                <SelectItem value={profession.name}>
-                  {profession.name}
-                </SelectItem>
-              </React.Fragment>
-            )
-          })}
-        </SelectContent>
-      </Select>
+      <h1 className='text-center'>Professions</h1>
+      <div className='flex h-screen w-screen flex-col items-center'>
+        <div className='flex w-3/4 bg-purple-100'>
+          <Select>
+            <SelectTrigger>
+              <SelectValue placeholder='Professions' />
+            </SelectTrigger>
+            <SelectContent>
+              {professions.map((profession: Professions, index: number) => {
+                return (
+                  <React.Fragment key={index}>
+                    <SelectItem value={profession.name}>
+                      {profession.name}
+                    </SelectItem>
+                  </React.Fragment>
+                )
+              })}
+            </SelectContent>
+          </Select>
+        </div>
+      </div>
     </>
   )
 }
