93c7258ffcfe2f73ca08b4190d509f7544cbca8d
Tymee Kong <tymeekong562@gmail.com>
2024-03-16 00:14:45 -0700

    Made basic outline for navbar

--- app/home/nav.tsx
+++ app/home/nav.tsx
@@ -1,37 +1,34 @@
-'use client'
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-} from '@/components/ui/navigation-menu'
 import Logo from '../../Images/logo.png'
 import React from 'react'
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import Image from 'next/image'
+import { Label } from '@/components/ui/label'
+import { Button } from '@/components/ui/button'
 
 const Nav: React.FC = () => {
   return (
-    <div className='flex w-screen bg-purple-100'>
+    <div className='flex w-screen'>
       <div className='w-1/2 '>
-        <Avatar className=''>
-          <AvatarImage src='https://github.com/shadcn.png' />
-          <AvatarFallback>Logo</AvatarFallback>
-        </Avatar>
-        <h1 className='inline'>Company Name</h1>
+        <Image
+          className='inline rounded-full'
+          src={Logo}
+          height={50}
+          width={50}
+          alt='logo'
+        />
+        <h1 className='inline p-0 m-0'>Gameplay Tabletop</h1>
       </div>
-      <div className='w-1/2 bg-white'>
-        <NavigationMenu>
-          <NavigationMenuList>
-            <NavigationMenuItem>
-              <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
-              <NavigationMenuContent>
-                <NavigationMenuLink>Link</NavigationMenuLink>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
-          </NavigationMenuList>
-        </NavigationMenu>
+      <div className='text-right w-1/2 bg-white'>
+        <a>
+          <Label>Home |</Label>
+        </a>
+        <a>
+          <Label> About |</Label>
+        </a>
+        <a>
+          <Label> Contact</Label>
+        </a>
+        <Label> Login </Label>
+        <Button>Signup </Button>
       </div>
     </div>
   )
