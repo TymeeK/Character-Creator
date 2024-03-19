68bb997bec1b87d32cb6a4b4ffedfa2399fb2dd7
Tymee Kong <tymeekong562@gmail.com>
2024-03-18 23:32:16 -0700

    Started to add onclick handler

--- app/dg/creator/professions/optionalskills.tsx
+++ app/dg/creator/professions/optionalskills.tsx
@@ -1,20 +1,34 @@
 import { Professions } from '@/Delta Green/Types/professions'
-import React, { Fragment } from 'react'
+import React, { Fragment, useRef, useState } from 'react'
 import { Checkbox } from '@/components/ui/checkbox'
 import { Label } from '@/components/ui/label'
+import { CheckedState } from '@radix-ui/react-checkbox'
 
 type props = {
   profession: Professions | undefined
 }
 const OptionalSkills: React.FC<props> = ({ profession }) => {
   const optional = profession ? profession.options : undefined
+  const [disabled, setDisabled] = useState<Boolean>(false)
+  const [isChecked, setIsChecked] = useState<CheckedState>(false)
+  const numCheckBoxesClicked = useRef(0)
+
+  const checkCheckBox: React.MouseEventHandler<HTMLButtonElement> = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  ): void => {
+    console.log(e)
+    console.log('')
+  }
   return (
     <>
       <h2 className='text-center'>Optional Skills</h2>
       {optional ? (
-        <li>
-          Pick {optional.num} skills from the {optional.skills.length}
-        </li>
+        <>
+          <Label>
+            Pick {optional.num} skills from the {optional.skills.length}
+          </Label>
+          <br />
+        </>
       ) : (
         <></>
       )}
@@ -22,7 +36,11 @@ const OptionalSkills: React.FC<props> = ({ profession }) => {
         optional.skills.map((element, index) => {
           return (
             <>
-              <Checkbox key={index} />
+              <Checkbox
+                key={index}
+                onClick={checkCheckBox}
+                checked={isChecked}
+              />
               <Label>{element.name}</Label>
               <br />
             </>
